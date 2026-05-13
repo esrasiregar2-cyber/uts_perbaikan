9579c733cd81fb6547643dbbe68358b8aa098a49
Esra Siregar <esrasiregar2@gmail.com>
2026-05-13 13:50:38 +0700

    Membuat Model Kendaraan

--- app/Models/Kendaraan.php
+++ app/Models/Kendaraan.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Kendaraan extends Model
 {
-   
+    use HasFactory;
+
+    protected $fillable = [
+        'plat_nomor',
+        'nama_pemilik',
+        'merk_kendaraan',
+        'keluhan'
+    ];
 }
